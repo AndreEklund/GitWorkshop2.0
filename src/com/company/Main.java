e00dd4eb91fc2ecce8d6f7f574db66a102bf2ed7
filipornling <filipornling@gmail.com>
2021-03-22 11:14:45 +0100

    good

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-      System.out.printnl("Tjena Grabbar!");
+      System.out.println("Tjena Grabbar!");
         for (int i = 0; i < 10; i++) {
             System.out.println("Hallå");
         }
