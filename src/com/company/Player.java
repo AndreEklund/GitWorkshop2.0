0af606a8d0f574fd6175d08d911507e6db3f35d1
benny1051 <benny1051@gmail.com>
2021-03-22 11:01:31 +0100

    Player class
    First version

---
+++ src/com/company/Player.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Player {
+
+    String Name;
+    double Score;
+
+    public  Player(String name, double score){
+        this.Name=name;
+        this.Score=score;
+    }
+}
