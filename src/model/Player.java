a1d47ef96c11147d1972755aefcdde7e0677eac1
benny1051 <benny1051@gmail.com>
2021-03-31 10:41:35 +0200

    Update Player.java

--- src/model/Player.java
+++ src/model/Player.java
@@ -35,24 +35,4 @@ public class Player {
 
 //<<Allt nedanför ska vara i en controllerklass>>
 
- // private List<Player> players = new ArrayList<>();// lista med spelare som vunnit
- //     private int numWinners=0;// Antalet personer som vunnit.
-
- // players.add(new Player(winner, countTotalGuess)); // addera spelarens namn med string och dens antal gissningar(poäng) till array av vinnare.
- //                 numWinners++;// incrementera antalet vinnare.
  //
- //
- //                 Collections.sort(players, new Comparator<Player>() {// Sortera vinnarna med varandra för att se vem som har bäst score. Sätt den längst upp.
- //                     @Override
- //                     public int compare(Player o1, Player o2) {
- //                         return o2.getScore() > o1.getScore() ? -1 : 1;
- //                     }
- //                 });
- //
- //                if (numWinners>10) {// Om fler än 10 spelare efter sortering. Ta bort sista spelaren ur listan.
- //                    players.remove(10);
- //                }
- //                 view.updateArray();// updatera listan som syns på vinnare i JList.
- //             }
- //
- //         }
