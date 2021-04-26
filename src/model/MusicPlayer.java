2723d0746a1303e887b842bb8a315dfb822c9af7
benny1051 <benny1051@gmail.com>
2021-04-26 08:06:47 +0200

    La till en musikspelarklass

---
+++ src/model/MusicPlayer.java
@@ -0,0 +1,36 @@
+package model;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import javazoom.jl.decoder.JavaLayerException;
+import javazoom.jl.player.Player;
+
+public class MusicPlayer {
+    private Player player;
+    private String filename; // The String that is the selected song
+
+
+    public MusicPlayer(String filename) {
+
+        this.filename=filename;
+    }
+
+public void playMusic(){
+      try {
+        player = new Player((new FileInputStream("Music/DingDong.mp3")));
+    } catch (JavaLayerException | FileNotFoundException e) {
+        e.printStackTrace();
+    }
+            try {
+        player.play();
+        System.out.println("Song is playing");
+
+    } catch (JavaLayerException e) {
+        e.printStackTrace();
+    }
+}
+
+public void stopMusic(){
+        player.close();
+}
+}
