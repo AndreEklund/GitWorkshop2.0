4a7eec35c0b427ff344d105346b037a716132a21
filipornling <filipornling@gmail.com>
2021-04-29 15:20:27 +0200

    Reset

--- src/view/OptionButtonPane.java
+++ src/view/OptionButtonPane.java
@@ -6,6 +6,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
@@ -18,7 +19,7 @@ public class OptionButtonPane extends GridPane {
 
     private Image imageStart;
     private Image imageMeny;
-    private String imageUrl = "files/Startgame.png";
+    private String imageUrl = "files/Stage1.png";
     private String imageUrlMeny = "files/Menybutton.png";
     private Main main;
     private MapCreator mapCreator;
@@ -26,28 +27,24 @@ public class OptionButtonPane extends GridPane {
     public OptionButtonPane(MapCreator mapCreator,Main main) throws FileNotFoundException {
         this.main=main;
         this.mapCreator=mapCreator;
-        FileInputStream inputStream = new FileInputStream(imageUrl);
-        imageStart = new Image(inputStream);
+
+        File file = new File(imageUrl);
+        imageStart = new Image(file.toURI().toString());
+
+
         ImageView imageView = new ImageView(imageStart);
         imageView.setFitHeight(50);
         imageView.setFitWidth(100);
 
-        inputStream = new FileInputStream(imageUrlMeny);
+        FileInputStream inputStream = new FileInputStream(imageUrlMeny);
         imageMeny = new Image(inputStream);
 
+        ImageView image = new ImageView(imageStart);
 
-        BackgroundImage bimage = new BackgroundImage(imageStart, BackgroundRepeat.NO_REPEAT,BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER,new BackgroundSize(imageStart.getWidth(), imageStart.getHeight(),true,true,true,false));
         BackgroundImage bimage2 = new BackgroundImage(imageMeny,BackgroundRepeat.NO_REPEAT,BackgroundRepeat.NO_REPEAT,BackgroundPosition.CENTER,new BackgroundSize(imageMeny.getWidth(),imageMeny.getHeight(),true,true,true,false));
 
+        add(image,0,1);
 
-        Button startGame = new Button();
-        startGame.setGraphic(imageView);
-
-        Background background = new Background(bimage);
-        startGame.setBackground(background);
-
-        add(startGame,0,0);
-        startGame.setOnMouseClicked(e ->startGameClicked(e));
         Button mute = new Button();
         ImageView imageView1 = new ImageView(imageMeny);
         mute.setGraphic(imageView1);
@@ -56,13 +53,10 @@ public class OptionButtonPane extends GridPane {
         
         mute.setBackground(background1);
         mute.setOnMouseClicked(e -> Mainmenyclicked(e));
-        add(mute,0,1);
+        add(mute,0,0);
     }
 
-    private void startGameClicked(MouseEvent e) {
-        System.out.println("Startgame Clicked");
-        mapCreator.start();
-    }
+
 
     private void Mainmenyclicked(MouseEvent e){
         System.out.println("Main Menu clicked");
