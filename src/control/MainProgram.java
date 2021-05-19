69da4ffda72fe4c5a5dc7c03809bcb70137b549f
filipornling <81147512+filipornling@users.noreply.github.com>
2021-05-19 12:32:40 +0200

    yes

--- src/control/MainProgram.java
+++ src/control/MainProgram.java
@@ -8,6 +8,7 @@ import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import model.Maps.World3Maps;
 import model.MazeGeneration.GenerateNextLevel;
 import model.MapCreator;
 import model.Maps.World1Maps;
@@ -48,6 +49,7 @@ public class MainProgram extends Application {
     private GenerateNextLevel generateNextLevel;
     private World1Template world1Template;
     private World1Maps world1Maps;
+    private World3Maps world3Maps;
 
 
     @Override
@@ -72,10 +74,11 @@ public class MainProgram extends Application {
         mainWindow.setOnCloseRequest(windowEvent -> System.exit(0));
         
         mazeGenerator = new MazeGenerator(10, true);
+        world3Maps = new World3Maps();
         world1Maps = new World1Maps();
 
         generateNextLevel = new GenerateNextLevel(this, mainPaneRandomMaze, mazeGenerator);
-        world1Template = new World1Template(world1Maps.getLevel11(), 1, 3, this);
+        world1Template = new World1Template(world3Maps.getLevel32(), 1, 3, this);
 
 
         mapTemplate = new MapTemplate(mazeGenerator.getMaze(),this, generateNextLevel);
