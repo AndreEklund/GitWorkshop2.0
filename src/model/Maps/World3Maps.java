c538e3a0a27a13e1d605a03dcf44caf304627842
filipornling <81147512+filipornling@users.noreply.github.com>
2021-05-19 12:03:59 +0200

    Create World3Maps.java

---
+++ src/model/Maps/World3Maps.java
@@ -0,0 +1,41 @@
+package model.Maps;
+
+public class World3Maps {
+
+    private int[][] level31 = {{0, 0, 0, 0, 0, 0, 1, 3},
+                               {0, 0, 1, 1, 4, 1, 1, 1},
+                               {0, 0, 1, 0, 0, 0, 0, 0},
+                               {0, 4, 1, 1, 1, 0, 0, 0},
+                               {0, 0, 0, 0, 1, 0, 0, 0},
+                               {0, 0, 0, 1, 1, 1, 4, 0},
+                               {1, 4, 1, 1, 0, 0, 0, 0},
+                               {2, 1, 1, 0, 0, 0, 0, 0}};;
+
+    private int[][] level32;
+
+    private int[][] level33;
+
+    private int[][] level34;
+
+    private int[][] level35;
+
+    public int[][] getLevel11(){
+        return level31;
+    }
+
+    public int[][] getLevel12(){
+        return level32;
+    }
+
+    public int[][] getLevel13(){
+        return level33;
+    }
+
+    public int[][] getLevel24(){
+        return level34;
+    }
+
+    public int[][] getLevel15() {
+        return level35;
+    }
+}
