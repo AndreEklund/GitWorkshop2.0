359d0bd1f939b920a1e26c43e018350801b84c71
filipornling <81147512+filipornling@users.noreply.github.com>
2021-05-19 12:41:40 +0200

    Update World3Template.java

--- src/view/Campaign/World3Template.java
+++ src/view/Campaign/World3Template.java
@@ -1,4 +1,61 @@
 package view.Campaign;
 
+import control.MainProgram;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
+import java.awt.event.MouseListener;
+import java.io.File;
+import java.util.ArrayList;
+
 public class World3Template {
+
+
+    /**
+     * Author André Eklund
+     */
+    private MainProgram mainProgram;
+    private int[][] level;
+    private ArrayList<Label> collectibles = new ArrayList<>();
+    private ArrayList<Label> pickaxes = new ArrayList<>();
+    
+    private Image wall;
+    private Image path;
+    private Image border;
+    private Image goal;
+    private Image diamond;
+    private Image start;
+    private Image ghost;
+    private Image heart;
+    private boolean startButtonPressed;
+    private boolean allCollectiblesObtained;
+    private boolean wallDestroyed;
+    private int collectiblesObtained = 0;
+    private int squareSize;
+    private int currentLevel;
+    private int heartCrystals;
+    private Image pickAxeImage;
+    private boolean pickaxeObtained;
+    private ImageView imageView = new ImageView();
+
+
+    private File diamondSound = new File("files/sounds/Diamond1.mp3");
+    private Media diamondMedia = new Media(diamondSound.toURI().toString());
+    private MediaPlayer diamondPlayer = new MediaPlayer(diamondMedia);
+
+    private File deathSound = new File("files/sounds/MazegenDeath.mp3");
+    private Media deathMedia = new Media(deathSound.toURI().toString());
+    private MediaPlayer deathPlayer = new MediaPlayer(deathMedia);
+
+
+    private File startSound = new File("files/sounds/MazegenStart.mp3");
+    private Media startMedia = new Media(startSound.toURI().toString());
+    private MediaPlayer startPlayer = new MediaPlayer(startMedia);
+
+    private File goalSound = new File("files/sounds/MazegenGoal.mp3");
+    private Media goalMedia = new Media(goalSound.toURI().toString());
+    private MediaPlayer goalPlayer = new MediaPlayer(goalMedia);
 }
