e0a400892fd922f11dffa2688c7c20ee7df8639d
kvnaslund <naslundv@gmail.com>
2021-05-21 16:45:58 +0200

    Intro och menu

--- src/view/Menu/Intro.java
+++ src/view/Menu/Intro.java
@@ -1,31 +1,49 @@
 package view.Menu;
 
 import control.MainProgram;
+import javafx.animation.FadeTransition;
+import javafx.animation.Transition;
 import javafx.event.EventHandler;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
+import javafx.util.Duration;
 
 /**
  * Author: Viktor Näslund
  */
 
-public class Intro extends VBox {
+public class Intro extends Pane {
 
     private MainProgram mainProgram;
+    private Image spaceImage;
+    private Image mazeGen;
+    private Image mazeGenGlow;
+    private Image pressMouse;
 
     public Intro(MainProgram mainProgram){
         this.mainProgram = mainProgram;
         setBackground();
         addListener();
+        setupImages();
+        introAnimation();
+
     }
 
 
     public void setBackground(){
-        BackgroundImage myBI= new BackgroundImage(new Image("file:files/IntroPic.png",800,600,false,true),
+        BackgroundImage backgroundImage= new BackgroundImage(new Image("file:files/intropics/1.png",800,600,false,true),
                 BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
                 BackgroundSize.DEFAULT);
-        this.setBackground(new Background(myBI));
+        this.setBackground(new Background(backgroundImage));
+    }
+
+    public void setupImages(){
+        spaceImage = new Image("file:files/intropics/2.png", 800,600,false,false);
+        mazeGen = new Image("file:files/intropics/3.png", 800,600,false,false);
+        mazeGenGlow = new Image("file:files/intropics/4.png", 800,600,false,false);
+        pressMouse = new Image("file:files/intropics/5.png", 800,600,false,false);
     }
 
 
@@ -33,9 +51,62 @@ public class Intro extends VBox {
         this.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent mouseEvent) {
+
                 mainProgram.changeToMenu();
             }
         });
+    }
+
+
+    public void introAnimation() {
+        ImageView introView = new ImageView(spaceImage);
+        introView.setStyle("fx-background-color: transparent;");
+        FadeTransition ft = new FadeTransition(Duration.millis(3000), introView);
+
+        getChildren().add(introView);
+        ft.setFromValue(0);
+        ft.setToValue(1);
+        ft.play();
+
+
+        ImageView mazeGenView = new ImageView(mazeGen);
+        mazeGenView.setStyle("fx-background-color: transparent;");
+        mazeGenView.toFront();
+        FadeTransition ft2 = new FadeTransition(Duration.millis(2000), mazeGenView);
+        mazeGenView.setOpacity(0);
+        getChildren().add(mazeGenView);
+        ft2.setDelay(Duration.millis(2000));
+
+        ft2.setFromValue(0);
+        ft2.setToValue(1);
+        ft2.play();
+
+        ImageView mazeGenGlowView = new ImageView(mazeGenGlow);
+        mazeGenGlowView.setStyle("fx-background-color: transparent;");
+        mazeGenGlowView.toFront();
+        FadeTransition ft3 = new FadeTransition(Duration.millis(1500), mazeGenGlowView);
+        mazeGenGlowView.setOpacity(0);
+        getChildren().add(mazeGenGlowView);
+        ft3.setDelay(Duration.millis(3500));
+
+        ft3.setFromValue(0);
+        ft3.setToValue(1);
+        ft3.play();
+
+        ImageView pressMouseView = new ImageView(pressMouse);
+        pressMouseView.setStyle("fx-background-color: transparent;");
+        pressMouseView.toFront();
+        FadeTransition ft4 = new FadeTransition(Duration.millis(1500), pressMouseView);
+        ft4.setCycleCount(Transition.INDEFINITE);
+        pressMouseView.setOpacity(0);
+        getChildren().add(pressMouseView);
+        ft4.setDelay(Duration.millis(5500));
+
+        ft4.setFromValue(0);
+        ft4.setToValue(1);
+        ft4.play();
+
+
     }
 
 }
--- src/view/Menu/Menu.java
+++ src/view/Menu/Menu.java
@@ -14,13 +14,30 @@ import java.io.FileNotFoundException;
 
 public class Menu extends VBox {
     private MainProgram mainProgram;
+    private Image campaign;
+    private Image campaignResize;
+    private Image randomize;
+    private Image randomizeResize;
+    private Image help;
+    private Image helpResize;
+    private Image mazegen;
 
     public Menu(MainProgram mainProgram){
         this.mainProgram = mainProgram;
         setBackground();
+        setupImages();
         addButtons();
     }
 
+    public void setupImages(){
+        mazegen = new Image("file:files/texts/MazegenTitel.png", 800, 600, false,false);
+        campaign = new Image("file:files/texts/Campaign.png", 250, 30, false, false);
+        campaignResize = new Image("file:files/texts/Campaign.png", 275, 33, false, false);
+        randomize = new Image("file:files/texts/Randomize.png", 250, 30, false, false);
+        randomizeResize = new Image("file:files/texts/Randomize.png", 275, 33, false, false);
+        help = new Image("file:files/texts/Help.png", 250, 30, false, false);
+        helpResize = new Image("file:files/texts/Help.png", 275, 33, false, false);
+    }
 
     public void setBackground(){
         BackgroundImage menuBackground = new BackgroundImage(new Image("file:files/MenuBackground.jpg",800,600,false,true),
@@ -30,31 +47,72 @@ public class Menu extends VBox {
     }
 
     public void addButtons(){
-        Button button1 = new Button("");
-        Image btnStart = new Image("file:files/texts/Campaign.png", 250, 30, false, true);
-        button1.setGraphic(new ImageView(btnStart));
-        button1.setOnAction(e -> {
+        ImageView mazegenView = new ImageView(mazegen);
+        mazegenView.setStyle("fx-background-color: transparent;");
+
+
+        ImageView campaignView = new ImageView(campaign);
+        campaignView.setStyle("fx-background-color: transparent;");
+        campaignView.setTranslateX(275);
+        campaignView.setTranslateY(200);
+        campaignView.toFront();
+        campaignView.setOnMouseEntered(e -> {
+            campaignView.setImage(campaignResize);
+            campaignView.setTranslateX(263);
+            campaignView.setTranslateY(197);
+        });
+        campaignView.setOnMouseExited(e -> {
+            campaignView.setImage(campaign);
+            campaignView.setTranslateX(275);
+            campaignView.setTranslateY(200);
+        });
+        campaignView.setOnMouseClicked(e -> {
             try {
                 mainProgram.changeToCampaign();
             } catch (FileNotFoundException fileNotFoundException) {
                 fileNotFoundException.printStackTrace();
             }
         });
-        button1.setTranslateX(250);
-        button1.setTranslateY(125);
-        Button button2 = new Button("");
-        Image btnMapCreator = new Image("file:files/texts/Randomize.png", 250, 30, false, true);
-        button2.setGraphic(new ImageView(btnMapCreator));
-        button2.setOnAction(e -> mainProgram.changeToRandomize());
-        button2.setTranslateX(250);
-        button2.setTranslateY(150);
-        Button button3 = new Button("");
-        Image btnHelp = new Image("file:files/texts/Help.png", 250, 30, false, true);
-        button3.setGraphic(new ImageView(btnHelp));
-        button3.setOnAction(e -> mainProgram.changeToHelp());
-        button3.setTranslateX(250);
-        button3.setTranslateY(175);
-        this.getChildren().addAll(button1,button2,button3);
+
+        ImageView randomizeView = new ImageView(randomize);
+        randomizeView.setStyle("fx-background-color: transparent;");
+        randomizeView.setTranslateX(275);
+        randomizeView.setTranslateY(225);
+        randomizeView.toFront();
+        randomizeView.setOnMouseEntered(e -> {
+            randomizeView.setImage(randomizeResize);
+            randomizeView.setTranslateX(263);
+            randomizeView.setTranslateY(222);
+        });
+        randomizeView.setOnMouseExited(e -> {
+            randomizeView.setImage(randomize);
+            randomizeView.setTranslateX(275);
+            randomizeView.setTranslateY(225);
+        });
+        randomizeView.setOnMouseClicked(e -> {
+            mainProgram.changeToRandomize();
+        });
+
+        ImageView helpView = new ImageView(help);
+        helpView.setStyle("fx-background-color: transparent;");
+        helpView.setTranslateX(275);
+        helpView.setTranslateY(250);
+        helpView.toFront();
+        helpView.setOnMouseEntered(e -> {
+            helpView.setImage(helpResize);
+            helpView.setTranslateX(263);
+            helpView.setTranslateY(247);
+        });
+        helpView.setOnMouseExited(e -> {
+            helpView.setImage(help);
+            helpView.setTranslateX(275);
+            helpView.setTranslateY(250);
+        });
+        helpView.setOnMouseClicked(e -> {
+            mainProgram.changeToHelp();
+        });
+
+        this.getChildren().addAll(campaignView,randomizeView,helpView,mazegenView);
     }
 
 }
